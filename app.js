8623e789babf56c255711be25692ba1ce10d8126
Ashish Kumar <davidashish1996@gmail.com>
2024-05-10 15:25:49 +0530

    This is first Changes in React

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-const heading = React.createElement("h1",{id:"heading1"},"Hello from React JS");
+/*
+<div id="root">
+    <div id="parent">
+    <div id="children">
+    <h1 id="heading1">This is Heading Tags</h1>
+    <h2 id="heading2">This is Heading Tags</h2>
+    </div>
+    </div>
+</div>
+
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "children" }, [
+    React.createElement("h1", { id: "heading1" }, "This is Heading Tags"),
+    React.createElement(
+      "h2",
+      { id: "heading2" },
+      "This is second Heading Tags"
+    ),
+  ])
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(parent)
